dabdfe3b874ea849e7939f465b696c0eec978502
SETSOFIA NUSETOR <nusetorsetsofia101@gmail.com>
2025-06-28 15:20:38 +0000

    feat: Add custom app strings
    This commit introduces a new file `custom_app_strings.dart` to store various string constants used throughout the application.
    
    - Added strings for authentication screens, including login and sign-up headers, subheadings, and button texts.
    - Included general strings like "OR".

---
+++ lib/shared/app_strings/custom_app_strings.dart
@@ -0,0 +1,20 @@
+class CustomAppStrings {
+
+  //Authentication screen strings
+  static String loginHeaderText = "Welcome Back!";
+  static String loginSubHeadingText = "Login now to access amazing hostels made for only you!";
+  static String loginButtonText = "Login";
+
+  static String continueWithGoogleText = "Continue With Google";
+  static String donNotHaveAccountText = "Don't have an account?";
+  static String registerText = "Register";
+
+
+  static String signUpHeaderText = "Create an account";
+  static String signUpSubHeadingText = "Register now to access amazing hostels made for only you!";
+  static String signupButtonText = "Sign Up";
+
+  //General
+  static String orText = "OR";
+
+}
