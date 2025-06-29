9cd34fd8b4bf0515bac758a6612474977f1cd4aa
SETSOFIA NUSETOR <nusetorsetsofia101@gmail.com>
2025-06-29 22:52:08 +0000

    feat: Add dummy hostel image assets
    This commit introduces paths for three dummy hostel images. These assets will be used as placeholders or for initial UI development related to hostel listings.
    
    - Added `hostel1`, `hostel2`, and `hostel3` to `AppAssets`.

--- lib/shared/app_assets/app_assets.dart
+++ lib/shared/app_assets/app_assets.dart
@@ -2,4 +2,9 @@ class AppAssets {
 
   static String googleIcon = "assets/icons/auth/google-icon.png";
 
+  //Dummy hostel assets
+  static String hostel1 = "assets/images/dummy_hostels/hostel-1.png";
+  static String hostel2 = "assets/images/dummy_hostels/hostel-2.jpg";
+  static String hostel3 = "assets/images/dummy_hostels/hostel-3.png";
+
 }
