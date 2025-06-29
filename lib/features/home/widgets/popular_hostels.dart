4557ef447e7069e1f4263b53cadaabb802bc52fe
SETSOFIA NUSETOR <nusetorsetsofia101@gmail.com>
2025-06-29 22:55:29 +0000

    feat: Add popular hostels section to home screen
    This commit introduces a "Popular Hostels" section to the home screen.
    
    - Created `PopularHostels` widget to display a horizontal list of hostels.
    - Used `PopularHostelCard` to render individual hostel information.
    - Added dummy hostel images to `assets/images/dummy_hostels/`.
    - Updated `pubspec.yaml` to include the new asset path.
    - Placeholder data is used for hostel details and will be replaced with API data later.

---
+++ lib/features/home/widgets/popular_hostels.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/cupertino.dart';
+import 'package:hostel_finder/features/home/widgets/popular_hostel_card.dart';
+import 'package:hostel_finder/shared/app_assets/app_assets.dart';
+import 'package:hostel_finder/shared/app_strings/custom_app_strings.dart';
+import 'package:hostel_finder/shared/custom_app_labels/custom_app_header_text.dart';
+
+class PopularHostels extends StatefulWidget {
+  const PopularHostels({super.key});
+
+  @override
+  State<PopularHostels> createState() => _PopularHostelsState();
+}
+
+class _PopularHostelsState extends State<PopularHostels> {
+
+
+
+  //Todo: this comes from the api response
+  final List<String> _assets = [AppAssets.hostel1, AppAssets.hostel2, AppAssets.hostel3];
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        CustomAppHeaderText(text: CustomAppStrings.popularHostelsString, size: 24),
+
+        SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Row(
+            children: [
+              ..._assets.map((asset) {
+                return Padding(
+                  padding: const EdgeInsets.only(right: 8.0),
+                  child: PopularHostelCard(
+                    hostelImageUrl: asset, //Todo: this comes from the api response
+                    averageRating: 4.6, //Todo: this comes from the api response
+                    totalRating: 334, //Todo: this comes from the api response
+                    hostelName: 'Kings Queens Hostel', //Todo: this comes from the api response
+                    hostelLocation: 'Adenta Municipality', //Todo: this comes from the api response
+                    hostelAmenities: ["Water 27/7", "Kitchen", "Free bed", "Learning room", "Basketball court"] //Todo: this comes from the api response
+                  ),
+                );
+              })
+            ],
+          ),
+        )
+      ],
+    );
+  }
+}
