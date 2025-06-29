6cd7e83d1be81ef4a87525eca2ccd3f5dac38b61
SETSOFIA NUSETOR <nusetorsetsofia101@gmail.com>
2025-06-29 14:24:43 +0000

    feat: Implement AppNavigator for simplified navigation
    This commit introduces the `AppNavigator` class, providing a static method `navigateTo` to simplify page navigation within the application.
    
    - The `navigateTo` method takes a `BuildContext` and a `Widget` (the page to navigate to) as arguments.
    - It uses `Navigator.of(context).push` with a `MaterialPageRoute` to transition to the new page.

---
+++ lib/core/naviagtor/app_navigator.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class AppNavigator {
+
+  static void navigateTo(BuildContext context, Widget page) => Navigator.of(context).push(MaterialPageRoute(builder: (builder) => page));
+}
